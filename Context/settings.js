0ae4be6ff32ad97a44e89f68e49bdbd1e1ec6f8d
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-24 08:35:12 +0200

    front-end

---
+++ Context/settings.js
@@ -0,0 +1,15 @@
+export const APP_PAGES = {
+  APP: {
+    HOME: "HOME",
+  },
+};
+
+export const COLORS = {
+  BACKGROUND: "#ffffff",
+  MAIN_COLOR: "#0d162f",
+  SUB_TEXT_COLOR: "#9ba2b0",
+  SUB_FONT_SIZE: 13,
+  MAIN_FONT_SIZE: 18,
+  LOSE_COLOR: "#fc6d77",
+  GAIN_COLOR: "#4cdbb5",
+};
